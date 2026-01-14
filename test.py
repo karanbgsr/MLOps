3f25b3f7ac984fd77b813474987b14ca13106f82
Gulati <karan.a.gulati@accenture.com>
2026-01-14 16:50:02 +0530

    Added comments

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+
+#Some important git commands
+
+# Git Clone: git clone <repository_url>
+# Git Add : git add . #(to stage all changes)
+# Git Commit: git commit -m "Your commit message"
+# Git Push: git push origin main #(or the branch you are working on)
+# Git Pull: git pull origin main #(or the branch you are working on)
