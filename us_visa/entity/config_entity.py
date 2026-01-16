f43c62455725a4216a28cf43157b9def1c0bbb63
Gulati <karan.a.gulati@accenture.com>
2026-01-16 14:03:22 +0530

    Last changes

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -2,3 +2,27 @@ import os
 from us_visa.constants import *
 from datetime import datetime
 from dataclasses import dataclass
+
+
+TIMESTAMP : str = datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
+
+#with dataclass wrapper you dont need to create a constructor with def __init__(self):
+# you can directly create class attributes
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name:str  = PIPLELINE_NAME
+    artifact_dir:str = os.path.join(ARTIFACT_DIR,TIMESTAMP)
+    timestamp:str  = TIMESTAMP
+
+
+#Naming convention <Name of the variable >: <Type> = <class>
+training_pipeline_config:TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+    collection_name:str = DATA_INGESTION_COLLECTION_NAME
